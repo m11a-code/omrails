03ea98c335167cb8a0b7e682614cbc597f0f8ee5
Matthew Ibarra <msibarr@gmail.com>
2013-09-25 22:42:23 -0400

    Updated Home and About pages, added content, linking to both pages, header, and footer.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Omrails::Application.routes.draw do
+  # get "pages/home"
+  get 'about' => 'pages#about'
+  root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +42,7 @@ Omrails::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
